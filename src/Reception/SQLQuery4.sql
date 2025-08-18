39b2cd1b37b132e50d94c52f92ecb5028679abab
zhishengchong <zhisheng2005@gmail.com>
2025-08-18 12:57:31 +0800

    Delete src/Reception directory

--- src/Reception/SQLQuery4.sql
+++
@@ -1,8 +0,0 @@
-﻿IF NOT EXISTS (
-    SELECT 1
-    FROM sys.indexes
-    WHERE name = 'UX_Receipt_BookingID'
-      AND object_id = OBJECT_ID('dbo.Receipt')
-)
-    CREATE UNIQUE INDEX UX_Receipt_BookingID
-    ON dbo.Receipt(BookingID);
