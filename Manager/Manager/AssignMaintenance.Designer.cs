f5fddffc5283020a1d3382912f85134458c14b5b
JustNPC123 <randomnessrandom101@gmail.com>
2025-08-19 23:43:57 +0800

    Small Changes v2

--- Manager/Manager/AssignMaintenance.Designer.cs
+++ Manager/Manager/AssignMaintenance.Designer.cs
@@ -95,9 +95,9 @@
             this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(322, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 36);
+            this.label1.Size = new System.Drawing.Size(185, 36);
             this.label1.TabIndex = 9;
-            this.label1.Text = "Add Users";
+            this.label1.Text = "Assign Staff";
             // 
             // btnAssign
             // 
